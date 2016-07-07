1b5d3888014d65817d6e8280a95e3a08d8ca6bb7
ruansheng <825307513@qq.com>
2016-07-08 00:45:06 +0800

    server config

--- src/com/rpc/main/Server.java
+++ src/com/rpc/main/Server.java
@@ -1,12 +1,24 @@
 package com.rpc.main;
 
+import java.util.Map;
+
+import com.rpc.utils.Config;
+
 /**
  * Created by ruansheng on 16/7/1.
  */
 public class Server {
-
+	
     public static void main(String[] args) throws Exception {
-        int port = 8080;
-        new Connector().bind(port);
+    	String filePath = "res/server.properties";
+    	Map<String, Object> config = new Config().getServerConfig(filePath);
+    	if(config == null) {
+    		System.out.println("get server config ");
+    	} else {
+    		String serverName = config.get("serverName").toString();
+        	int port = Integer.parseInt(config.get("port").toString());
+        	System.out.println("server " + serverName + " listen port " + port +" is running...");
+        	new Connector().bind(port);
+    	}
     }
 }
---
+++ src/com/rpc/utils/Config.java
@@ -0,0 +1,37 @@
+package com.rpc.utils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class Config {
+	
+	/**
+	 * get server config
+	 * @param filePath
+	 * @return
+	 */
+	public Map<String, Object> getServerConfig(String filePath) {
+		Map<String, Object> config = new HashMap<String, Object>();
+		InputStream in = null;
+		try {
+			in = new BufferedInputStream(new FileInputStream(filePath));
+			Properties prop = new Properties();
+			prop.load(in);
+			
+			String serverName = prop.getProperty("serverName").trim();   
+            String port = prop.getProperty("port").trim();
+            config.put("serverName", serverName);
+            config.put("port", port);
+		} catch (Exception e) {
+		    config = null;
+		}
+        
+		return config;
+	}
+}
